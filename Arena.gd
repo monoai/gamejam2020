9f16a6bd0e5a3bc6545726f425c1478968ae6f67
JayAiJoe <60511525+JayAiJoe@users.noreply.github.com>
2020-02-01 10:51:30 +0800

    Added a bunch of stuff
    Yeah

--- Arena.gd
+++ Arena.gd
@@ -1,17 +1,12 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-signal moved_player1(moveArray)
-signal moved_player2(moveArray)
-
-var moveP1 = [0,0,0,0]
-var moveP2 = [0,0,0,0]
-
-
-
-
-	
-
+func _ready():
+	$Player1.connect("interact_P1",$Turrets/Tower1,"repair")
+	$Player1.connect("interact_P1",$Turrets/Tower2,"repair")
+	$Player1.connect("interact_P1",$Turrets/Tower3,"repair")
+	$Player1.connect("interact_P1",$Turrets/Tower4,"repair")
+	$Player2.connect("interact_P2",$Turrets/Tower5,"repair")
+	$Player2.connect("interact_P2",$Turrets/Tower6,"repair")
+	$Player2.connect("interact_P2",$Turrets/Tower7,"repair")
+	$Player2.connect("interact_P2",$Turrets/Tower8,"repair")
--- Player1.gd
+++ Player1.gd
@@ -1,32 +1,48 @@
 extends KinematicBody2D
 
+class_name player1
+
 const ACCELERATION = 100
 const MAX_SPEED = 750
 
-var SPEED = 10
+var SPEED = 25
 var center
-var move = Vector2(0,0)
 var displacement
 
-onready var map_size = get_viewport().size
+var RES_MAX = 5
+
+var inventory = [0,0,0,0,0,0]
+
+signal interact_P1
 
 
 func _ready():
 	position = Vector2(-300,0)
-	center = map_size/2
  
+func _input(event):
+	if event.is_action_pressed("interact1"):
+		emit_signal("interact_P1")
 
 func _physics_process(delta):
-	move = Vector2(0,0)
+	var move = Vector2()
 	if Input.is_action_pressed("right_1"):
-		move.x = 1
-	elif Input.is_action_pressed("left_1"):
-		move.x = -1
-	elif Input.is_action_pressed("down_1"):
-		move.y = 1
-	elif Input.is_action_pressed("up_1"):
-		move.y = -1
+		move.x += 1
+	if Input.is_action_pressed("left_1"):
+		move.x -= 1
+	if Input.is_action_pressed("down_1"):
+		move.y += 1
+	if Input.is_action_pressed("up_1"):
+		move.y -= 1
+		
+	if move.length() > 0:
+		move = move.normalized() * SPEED * delta
 	
 	displacement = position + SPEED*move
-	displacement.x = clamp(displacement.x,-map_size.x/2,0)
+	displacement.x = min(displacement.x,0)
 	position = displacement.clamped(600)
+
+func update_storage():
+	for i in inventory:
+			i = clamp(i,0,RES_MAX)
+	$Res_Storage.update_text()
+	
